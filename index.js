6bb7e1b7b7b02fc67247455fb27a6ab4f04c7cbe
MartinKamauMaina888 <martin.maina01@student.moringaschool.com>
2022-11-23 11:32:46 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,76 @@
+1+80
+81
+60-40
+20
+2*3.4
+6.8
+5.0/2.5
+2
+
+
+add(+);
+subtract(-);
+multiply(*);
+divide(/)
+
+let number=5;
+undefined
+number++
+5
+number;
+6
+number--
+6
+number;
+5
+number =5
+5
+number +=3
+8
+number -=2
+6
+number *=10
+60
+number /=5
+12
+number =10;
+10
+function add5(){
+    return (number +=5);
+}
+undefined
+function divideBy3() {
+  return (number /= 3);
+}
+
+divideBy3(); //=> 3.3333333333333335
+
+add5(); //=> 8.333333333333334
+
+8.333333333333334
+number =10;
+10
+add5();
+15
+divideBy3();
+5
+parseInt("2" ,10);
+2
+
+parseInt("2.2222" ,10);
+2
+parseInt("nonsense!" ,10);
+NaN
+parseFloat("80.123999");
+80.123999
+ function makeInt(string){return parseInt("3", 10);} makeInt()
+3
+ funcntion preserveDecimal(string){return parseFloat("3.333");} preserveDecimal()
+REPL14:1
+funcntion preserveDecimal(string){return parseFloat("3.333");} preserveDecimal()
+          ^^^^^^^^^^^^^^^
+
+SyntaxError: Unexpected identifier
+ function preserveDecimal(string){return parseFloat("3.333");} preserveDecimal()
+3.333
+ 
